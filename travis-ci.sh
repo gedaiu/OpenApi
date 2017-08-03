6f9627606d1dc83eec7fbc05ea619e1941bd519f
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-08-04 00:37:29 +0300

    add travis

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,18 +3,13 @@ set -e -x -o pipefail
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --combined --arch=x86
+	dub build --arch=x86 --compiler=$DC
 	dub clean --all-packages
 fi
 
 # test for successful release build
-dub build --combined -b release --compiler=$DC
+dub build -b release --compiler=$DC
 dub clean --all-packages
 
 # run unit tests
 dub test --compiler=$DC
-
-# run a build for unit-threaded
-if ! $($DC --version | grep -q 1.0.0); then
-  dub --root=test/unit-threaded --compiler=$DC --arch=x86_64
-fi
